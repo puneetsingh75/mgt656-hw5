f052cad5c7e2cd2a3598f53dde438ffeb095e8dc
puneetsingh75 <2puneetsingh@gmail.com>
2015-10-09 23:42:51 +0000

    added nickname route

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/nickname', function(request, response){
+    response.send('Puneet');
+});
+
+
 app.listen(process.env.PORT || 4000);
